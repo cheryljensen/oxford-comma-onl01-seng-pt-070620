e691f6f3cd17c2eea82b5f73ec430ec74e4240de
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-14 01:04:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
- case array.length
- when 1
+ if array.length == 1
    array.join()
- when 2
+ elsif array.length == 2
    array.join(" and ")
- when 3
+ else
    array[0..-2].join(", ") + ", and " + "#{array[-1]}"
 end
 end
